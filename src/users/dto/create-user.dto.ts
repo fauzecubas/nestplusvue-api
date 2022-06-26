e8b8030dd8bda1af38efc8bedab445e44a84f55c
Fauze Cubas <fauzehenrique01@gmail.com>
2022-06-26 17:22:52 -0300

    feat: create and update user dto

---
+++ src/users/dto/create-user.dto.ts
@@ -0,0 +1,10 @@
+import { IsNumber, IsString, MinLength } from 'class-validator';
+
+export class CreateUserDto {
+  @IsString()
+  @MinLength(3)
+  name: string;
+
+  @IsNumber()
+  age: number;
+}
